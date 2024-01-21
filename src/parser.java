3234c69025cac339818dcd5c5bf5a16c78c3511d
Yarden <yardenrs12@gmail.com>
2024-01-21 14:36:26 +0200

    trying to add Parser

--- src/parser.java
+++ src/parser.java
@@ -1,6 +1,12 @@
-public class parser {
+public class Parser{
 
+    public Parser(){
 
-    
+    }
+
+    //returns if there are more lines in the input
+    public bool hasMoreLines(){
+
+    }
     
 }
