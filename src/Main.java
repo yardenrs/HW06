54e4e48d6837826378b0f7d85bb4b0e1805fd341
YardenWSL <yardenrs12@gmail.com>
2024-01-24 09:38:35 +0200

    Updates

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
     public static void main (String[] args) throws IOException{
         
-        HackAssembler assembler = new HackAssembler(args[0]);
-        assembler.run();
+        if(args.length < 1)
+            return;
+        String path = args[0];
+        File file = new File(path);
+        if (file.isFile() && file.getName().endsWith(".asm")) {  // If it's a single .asm file, process it
+            HackAssembler assembler = new HackAssembler(path);
+            assembler.run();
+        }
+        else if (file.isDirectory()) { // If it's a directory, process all .asm files in the directory
+            File[] asmFiles = file.listFiles((dir, name) -> name.endsWith(".asm"));
+            if (asmFiles != null && asmFiles.length > 0) {    // Process each .asm file in the directory
+                for (File asmFile : asmFiles) {
+                    HackAssembler assembler = new HackAssembler(asmFile.getAbsolutePath());
+                    assembler.run();
+                }
+            }
+        }
+        else {
+            System.out.println("Invalid path or file type.");
+        }
+    }
     }
       
 }
